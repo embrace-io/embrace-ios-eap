f96af727f7e9103a1b88eb0447cf8fdd05952a00
Juan Pablo Crespi <juanpablocrespi99@gmail.com>
2020-08-07 21:07:01 -0300

    Add Swift Package Manager specification

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "Embrace",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "Embrace", 
+            targets: ["Embrace"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Embrace", 
+            path: "Embrace.xcframework")
+    ])
